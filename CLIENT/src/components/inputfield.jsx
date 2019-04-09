d40cef8b7d324391be7f1031f08a2307a591f241
José <jrsimog@gmail.com>
2019-04-08 21:24:31 -0400

    add components

---
+++ CLIENT/src/components/inputfield.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Input from "@material-ui/core/Input";
+
+class InputFile extends Component{
+
+
+    render(){
+        return (<Input type="file" />);
+    }
+   
+  // 
+}
+
+export default InputFile;
